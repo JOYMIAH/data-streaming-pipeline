bf9e22b4ef7b108f88b551c580dc1c9437dcd504
JOY MIAH <99273251+JOYMIAH@users.noreply.github.com>
2023-01-18 17:13:37 +0600

    3-Node,Broker

--- consumer.py
+++ consumer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
-CONFIRMED_ORDER_SENDING_TOPIC = "test"
-consumer = KafkaConsumer(CONFIRMED_ORDER_SENDING_TOPIC ,bootstrap_servers={'192.168.61.28:9093'},
+CONFIRMED_ORDER_SENDING_TOPIC = "Generate_Order_Details"
+consumer = KafkaConsumer(CONFIRMED_ORDER_SENDING_TOPIC ,bootstrap_servers={"192.168.61.28:9093","192.168.61.28:9095","192.168.61.28:9097"},
 auto_offset_reset='earliest'
 # group_id='Consumer-Group-2'
 )
